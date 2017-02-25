690babb16201963e077243cd511e256799c75a91
Devon <loudywen@gmail.com>
2017-02-24 18:04:07 -0600

    bug fix
    to do:
    wait for sap up and build sap java object

--- src/main/java/com/devon/demo/main/service/TakeAction.java
+++ src/main/java/com/devon/demo/main/service/TakeAction.java
@@ -2,14 +2,21 @@ package com.devon.demo.main.service;
 
 import ai.api.model.Result;
 import com.devon.demo.main.AiDemoApplication;
+import com.devon.demo.util.Utility;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.context.request.async.DeferredResult;
 
-import java.util.concurrent.CompletableFuture;
+import java.net.URI;
 
 
 /**
@@ -35,6 +42,7 @@ public class TakeAction implements Action {
     private static final String ENTER_USER_ID = "enter.userid";
     private static final String PIN_INTENT = "pin";
     private TaskService taskService;
+    private Environment env;
 
     public TakeAction(Result result, String source, DummyDB dummyDB, RestTemplate restTemplate) {
         this.result = result;
@@ -42,6 +50,7 @@ public class TakeAction implements Action {
         this.dummyDB = dummyDB;
         this.restTemplate = restTemplate;
         this.taskService = (GetRoleDetailsAsyncTaskImpl) AiDemoApplication.getApplicationContext().getBean("getRoleDetailsAsyncTaskImpl");
+        this.env = AiDemoApplication.getApplicationContext().getEnvironment();
     }
 
 
@@ -78,9 +87,9 @@ public class TakeAction implements Action {
         if (dummyDB.findPin(result.getStringParameter(USER_ID), result.getIntParameter(PIN))) {
             // call SAP here later
 
-            DeferredResult<String> re = callSAPToGetRoleDetails(result.getStringParameter(USER_ID));
+            ResponseEntity<String> re = callSAPToGetRoleDetails(result.getStringParameter(USER_ID));
 
-            response = checkSourceForPinValid(re, "We will send you an Email with all your details");
+            response = checkSourceForPinValid(re.getBody().toString());
 
         } else {
             response = checkSource("Invalid PIN, do you want to try again?");
@@ -89,10 +98,10 @@ public class TakeAction implements Action {
         return response;
     }
 
-    private DeferredResult<String> callSAPToGetRoleDetails(String userid) {
-//        ResponseEntity<String> response2;
+    private ResponseEntity<String> callSAPToGetRoleDetails(String userid) {
+        ResponseEntity<String> response2;
         try {
-            /*String url = String.format(env.getProperty("sap.roles.details"), userid);
+            String url = String.format(env.getProperty("sap.roles.details"), userid);
 
             logger.debug("url: {}", url);
             URI uri = new URI(url);
@@ -103,19 +112,25 @@ public class TakeAction implements Action {
             HttpEntity<String> toRest2 = new HttpEntity<String>(headers);
             response2 = restTemplate.exchange(uri, HttpMethod.GET, toRest2, String.class);
             logger.debug(response2.getBody().toString());
-*/
 
-            logger.debug("Async call - enter");
-            DeferredResult<String> deferredResult = new DeferredResult<>();
+           /* logger.debug("Async call - enter");
+           *//* DeferredResult<String> deferredResult = new DeferredResult<>();
             CompletableFuture.supplyAsync(() -> taskService.buildResponse(userid))
                     .whenCompleteAsync((result, throwable) -> deferredResult.setResult(result.getBody()));
-            logger.debug("Async call - release");
+            *//*
+
+                logger.error(ex.getMessage(), ex);*/
+            /*    return response2;
+            }*/
+
+           /* logger.debug("Async call - release");*/
 
 
-            return deferredResult;
+            return response2;
         } catch (Exception ex) {
             logger.error(ex.getMessage(), ex);
-            return null;
+            response2 = new ResponseEntity<String>("SAP server is down, please come back later", HttpStatus.OK);
+            return response2;
         }
 
     }
@@ -131,14 +146,19 @@ public class TakeAction implements Action {
         return response;
     }
 
-    private String checkSourceForPinValid(DeferredResult responseMsgToSlack, String responseMsgToGoogleHome) {
+    private String checkSourceForPinValid(String responseMsgToSlack) {
         String response;
         if (source != null && source.equals("slack")) {
-            logger.debug("============{}",responseMsgToSlack.getResult());
-            response = FOR_RESPONSE_BACK_TO_SLACK_SENDER + responseMsgToSlack.getResult().toString();
+            logger.debug("============{}", responseMsgToSlack);
+            response = FOR_RESPONSE_BACK_TO_SLACK_SENDER + responseMsgToSlack;
 
         } else {
-            response = responseMsgToGoogleHome;
+
+            if (responseMsgToSlack.equals("SAP server is down, please come back later")) {
+                response = responseMsgToSlack;
+            } else {
+                response = "We will send you an Email with all your details";
+            }
         }
         return response;
     }
